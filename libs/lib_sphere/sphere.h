8d0679087f7634536c896a6a0b4062d3418c5b61
Валентин ЧерныхÐ <chernykhvalentin@gmail.com>
2025-09-10 19:00:35 +0300

    The implementation and tests for the Sphere class have been written.

--- libs/lib_sphere/sphere.h
+++ libs/lib_sphere/sphere.h
@@ -1,8 +1,20 @@
 // Copyright 2024 Chernykh Valentin
 
-#ifndef LIB_POINT_POINT_H_
-#define LIB_POINT_POINT_H_
+#ifndef LIB_SPHERE_SPHERE_H_
+#define LIB_SPHERE_SPHERE_H_
 
+#include "../lib_point3/point3.h"
 
+class Sphere {
+private:
+	Point3 _position;
+	int _radius;
+public:
+	Sphere();
+	Sphere(Point3 position, int radius);
 
-#endif  // LIB_POINT_POINT_H_
+	int get_radius();
+	Point3 get_position();
+};
+
+#endif  // LIB_SPHERE_SPHERE_H_
---
+++ tests/test_sphere.cpp
@@ -0,0 +1,6 @@
+// Copyright 2024 Chernykh Valentin
+
+#include <gtest/gtest.h>
+#include "../lib_sphere/sphere.h"
+
+#define EPSILON 0.000001
