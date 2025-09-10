3bdf13c8968f8f42e1a7f26395885a27507bbad9
Валентин ЧерныхÐ <chernykhvalentin@gmail.com>
2025-09-10 18:56:02 +0300

    The implementation and tests for the Point2 class have been written.

--- libs/lib_point2/point2.h
+++ libs/lib_point2/point2.h
@@ -1,8 +1,18 @@
 // Copyright 2024 Chernykh Valentin
 
-#ifndef LIB_POINT_POINT_H_
-#define LIB_POINT_POINT_H_
+#ifndef LIB_POINT2_POINT2_H_
+#define LIB_POINT2_POINT2_H_
 
+class Point2 {
+private:
+	int _x, _y;
+public:
+	Point2();
+	Point2(int x, int y);
+	Point2(const Point2& other);
 
+	float distance_to(const Point2&);
+};
 
-#endif  // LIB_POINT_POINT_H_
+#endif  // LIB_POINT_POINT2_H_
+
---
+++ tests/test_point2.cpp
@@ -0,0 +1,16 @@
+// Copyright 2024 Chernykh Valentin
+
+#include <gtest/gtest.h>
+#include "../lib_point2/point2.h"
+
+#define EPSILON 0.000001
+
+TEST(TestPoint2, distance_to_another) {
+    Point2 pos_1 = Point2(3, 4);
+    Point2 pos_2 = Point2(0, 0);
+    
+    float actual_result = pos_1.distance_to(pos_2);
+    float expected_result = 5;
+
+    EXPECT_EQ(expected_result, actual_result);
+}
