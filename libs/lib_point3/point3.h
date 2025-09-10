9de36ab565835751dc812d0aaaa802c9e3f8f050
Валентин ЧерныхÐ <chernykhvalentin@gmail.com>
2025-09-10 18:56:56 +0300

    The implementation and tests for the Point3 class have been written.

--- libs/lib_point3/point3.h
+++ libs/lib_point3/point3.h
@@ -1,8 +1,17 @@
 // Copyright 2024 Chernykh Valentin
 
-#ifndef LIB_POINT_POINT_H_
-#define LIB_POINT_POINT_H_
+#ifndef LIB_POINT3_POINT3_H_
+#define LIB_POINT3_POINT3_H_
 
+class Point3 {
+private:
+	int _x, _y, _z;
+public:
+	Point3();
+	Point3(int x, int y, int z);
+	Point3(const Point3& other);
 
+	float distance_to(const Point3&);
+};
 
-#endif  // LIB_POINT_POINT_H_
+#endif  // LIB_POINT3_POINT3_H_
