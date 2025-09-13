4084dc9b12cb6b0fa36e8758fa649d7017ea685e
Валентин ЧерныхÐ <chernykhvalentin@gmail.com>
2025-09-13 18:16:26 +0300

    The files mvector.h, mvector.cpp, and test_mvector.cpp have been added.

---
+++ libs/lib_mvector/mvector.h
@@ -0,0 +1,14 @@
+// Copyright 2025 Chernykh Valentin
+
+#ifndef LIBS_LIB_MVECTOR_MVECTOR_H_
+#define LIBS_LIB_MVECTOR_MVECTOR_H_
+
+#include "libs/lib_tvector/tvector.h"
+
+template<typename T>
+class MVector {
+
+
+};
+
+#endif  // LIBS_LIB_MVECTOR_MVECTOR_H_
