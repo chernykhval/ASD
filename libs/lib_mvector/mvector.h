c00b6467ee61364f3c3c474a3c03deaf2e126e2f
Valentin Chernykh <chernykhvalentin@gmail.com>
2025-09-21 20:00:36 +0300

    Constructors for the MVector class have been added and implemented.

--- libs/lib_mvector/mvector.h
+++ libs/lib_mvector/mvector.h
@@ -7,8 +7,33 @@
 
 template<typename T>
 class MVector {
+ private:
+    TVector<T> _data;
 
+ public:
+    MVector();
+    explicit MVector(size_t);
+    MVector(const MVector&);
 
+    size_t size() const;
 };
+
+template<typename T>
+MVector<T>::MVector() :_data() {}
+
+template<typename T>
+MVector<T>::MVector(size_t size) : _data(size) {
+    _data.shrink_to_fit();
+}
+
+template<typename T>
+MVector<T>::MVector(const MVector& other) {
+    _data = other._data;
+}
+
+template<typename T>
+size_t MVector<T>::size() const {
+    return _data.size();
+}
 
 #endif  // LIBS_LIB_MVECTOR_MVECTOR_H_
