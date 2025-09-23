35d7d2a9efa17a79ce41df59c0a84a7116418e34
Valentin Chernykh <chernykhvalentin@gmail.com>
2025-09-23 17:52:36 +0300

    The lib_matrix directory with library files has been added.

---
+++ libs/lib_matrix/matrix.h
@@ -0,0 +1,10 @@
+// Copyright 2025 Chernykh Valentin
+
+#ifndef LIBS_LIB_MATRIX_MATRIX_H_
+#define LIBS_LIB_MATRIX_MATRIX_H_
+
+template<typename T>
+class Matrix {
+};
+
+#endif  // LIBS_LIB_MATRIX_MATRIX_H_
