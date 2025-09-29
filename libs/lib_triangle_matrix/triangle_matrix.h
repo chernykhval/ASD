ef5ee7aa69a2770babd2e5dfaf2fa33314345a3c
Valentin Chernykh <chernykhvalentin@gmail.com>
2025-09-29 21:17:03 +0300

    Add triangle matrix library foundation files.

---
+++ libs/lib_triangle_matrix/triangle_matrix.h
@@ -0,0 +1,10 @@
+// Copyright 2025 Chernykh Valentin
+
+#ifndef LIBS_LIB_TRIANGLE_MATRIX_TRIANGLE_MATRIX_H_
+#define LIBS_LIB_TRIANGLE_MATRIX_TRIANGLE_MATRIX_H_
+
+template<typename T>
+class TriangleMatrix {
+};
+
+#endif  // LIBS_LIB_TRIANGLE_MATRIX_TRIANGLE_MATRIX_H_
