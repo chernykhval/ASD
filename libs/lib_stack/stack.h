f2f2a5a0b46ed14d83fb28e71f401d5255f05ba1
Valentin Chernykh <chernykhvalentin@gmail.com>
2025-10-11 21:10:28 +0300

    Add header and size constructor implementation.

--- libs/lib_stack/stack.h
+++ libs/lib_stack/stack.h
@@ -3,9 +3,33 @@
 #ifndef LIBS_LIB_STACK_STACK_H_
 #define LIBS_LIB_STACK_STACK_H_
 
+#include "libs/lib_tvector/tvector.h"
+
 template<typename T>
 class Stack {
  private:
+    TVector<T> _data;
+    size_t _top;
+
+public:
+    explicit Stack(size_t size);
+    Stack(const Stack& other);
+    Stack(Stack&& other);
+
+    void push(const T& value);
+    void push(T&& value);
+    void pop();
+    T& top();
+    const T& top() const;
+    T copy_top();
+    bool is_empty();
+    bool is_full();
+    void clear();
+};
+
+template<typename T>
+Stack<T>::Stack(size_t size) : _data(), _top(-1) {
+    _data.reserve(size);
 };
 
 #endif  // LIBS_LIB_STACK_STACK_H_
