6b5cb2b6d449f9c66f02c5db32323acb95f60b4c
Valentin Chernykh <chernykhvalentin@gmail.com>
2025-10-17 11:58:48 +0300

    Add lib_queue library files and test_queue test file

---
+++ libs/lib_queue/queue.h
@@ -0,0 +1,9 @@
+// Copyright 2025 Chernykh Valentin
+
+#ifndef LIBS_LIB_QUEUE_QUEUE_H_
+#define LIBS_LIB_QUEUE_QUEUE_H_
+
+class Queue {
+};
+
+#endif  // LIBS_LIB_QUEUE_QUEUE_H_
---
+++ tests/test_queue.cpp
@@ -0,0 +1,5 @@
+// Copyright 2025 Chernykh Valentin
+
+#include <gtest/gtest.h>
+
+#define EPSILON 0.000001
