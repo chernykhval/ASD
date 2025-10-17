3e516bda24fa53c88009678b726e564aac9c6abd
Valentin Chernykh <chernykhvalentin@gmail.com>
2025-10-17 12:29:39 +0300

    Add Queue class declaration

--- libs/lib_queue/queue.h
+++ libs/lib_queue/queue.h
@@ -3,7 +3,25 @@
 #ifndef LIBS_LIB_QUEUE_QUEUE_H_
 #define LIBS_LIB_QUEUE_QUEUE_H_
 
+#include <stdexcept>
+
+template<typename T>
 class Queue {
+ private:
+    T* _data;
+    size_t _size, _front, _count;
+
+ public:
+    explicit Queue(size_t size = 1);
+    Queue(const Queue& other);
+    ~Queue() noexcept;
+
+    void enqueue(const T& value);
+    void dequeue();
+    T& front();
+    bool is_empty() const noexcept;
+    bool is_full() const noexcept;
+    void clear() noexcept;
 };
 
 #endif  // LIBS_LIB_QUEUE_QUEUE_H_
