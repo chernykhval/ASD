6b9e022b3f3af5992726b7bb97c6b17ac8f92b40
Valentin Chernykh <chernykhvalentin@gmail.com>
2025-11-29 16:49:35 +0300

    feat: implement VarTable for variable management

---
+++ libs/lib_var_table/var_table.cpp
@@ -0,0 +1,44 @@
+// Copyright 2025 Chernykh Valentin
+
+#include "libs/lib_var_table/var_table.h"
+
+Variable::Variable() : value(0.0){
+}
+
+Variable::Variable(const std::string &_name, double _value)
+: name(_name), value(_value) {
+}
+
+void VarTable::add(const std::string& name, double value) {
+    int index = find_index(name);
+
+    if (index != -1) {
+        _vars[index].value = value;
+    } else {
+        _vars.push_back(Variable(name, value));
+    }
+}
+
+double VarTable::get(const std::string& name) const {
+    int index = find_index(name);
+
+    if (index != 1) {
+        return _vars[index].value;
+    }
+
+    throw std::runtime_error("Variable not found: " + name);
+}
+
+bool VarTable::contains(const std::string& name) const {
+    return find_index(name) != -1;
+}
+
+int VarTable::find_index(const std::string& name) const {
+    for (size_t i = 0; i < _vars.size(); ++i) {
+        if (_vars[i].name == name) {
+            return static_cast<int>(i);
+        }
+    }
+
+    return -1;
+}
---
+++ libs/lib_var_table/var_table.h
@@ -0,0 +1,30 @@
+// Copyright 2025 Chernykh Valentin
+
+#ifndef LIBS_LIB_VAR_TABLE_VAR_TABLE_H_
+#define LIBS_LIB_VAR_TABLE_VAR_TABLE_H_
+
+#include <string>
+#include "libs/lib_tvector/tvector.h"
+
+struct Variable {
+    std::string name;
+    double value;
+
+    Variable();
+    Variable(const std::string& _name, double _value);
+};
+
+class VarTable {
+private:
+    TVector<Variable> _vars;
+
+public:
+    void add(const std::string& name, double value);
+    double get(const std::string& name) const;
+    bool contains(const std::string& name) const;
+
+private:
+    int find_index(const std::string& name) const;
+};
+
+#endif  // LIBS_LIB_VAR_TABLE_VAR_TABLE_H_
