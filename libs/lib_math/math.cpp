0e8fa62b750b9e0312260c0dab6be80245295769
Valentin Chernykh <chernykhvalentin@gmail.com>
2025-11-30 19:02:21 +0300

    feat: introduce lib_math directory and Math namespace

---
+++ libs/lib_math/math.cpp
@@ -0,0 +1,24 @@
+// Copyright 2025 Chernykh Valentin
+
+#include <cmath>
+#include "libs/lib_math/math.h"
+
+double Math::sin(double x) {
+    return std::sin(x);
+}
+
+double Math::cos(double x) {
+    return std::cos(x);
+}
+
+double Math::tg(double x) {
+    return std::tan(x);
+}
+
+double Math::sqrt(double x) {
+    return std::sqrt(x);
+}
+
+double Math::ln(double x) {
+    return std::log(x);
+}
---
+++ libs/lib_math/math.h
@@ -0,0 +1,14 @@
+// Copyright 2025 Chernykh Valentin
+
+#ifndef LIBS_LIB_MATH_MATH_H_
+#define LIBS_LIB_MATH_MATH_H_
+
+namespace Math {
+    double sin(double x);
+    double cos(double x);
+    double tg(double x);
+    double sqrt(double x);
+    double ln(double x);
+};
+
+#endif  // LIBS_LIB_MATH_MATH_H_
